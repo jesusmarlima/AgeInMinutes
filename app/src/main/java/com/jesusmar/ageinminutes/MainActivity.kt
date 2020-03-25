42953fe46ea3924b84dd62d8baf7f02abfedf6bc
jesusmarlima <jesusmarlima@gmail.com>
2020-03-25 18:02:36 -0400

    add app

---
+++ app/src/main/java/com/jesusmar/ageinminutes/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.jesusmar.ageinminutes
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSelecteDate.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val month = myCalendar.get(Calendar.MONTH)
+
+
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener {
+                    view, year, month, dayOfMonth ->
+                    setInMinutesLabel(year, month, dayOfMonth)
+            }
+            , year
+            , month
+            , day)
+
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
+    }
+
+    private fun setInMinutesLabel(year: Int, month: Int, dayOfMonth: Int) {
+        val selectedDateAsString: String = "${dayOfMonth}/${month+1}/${year}"
+        textBirthday.text = selectedDateAsString
+        textMinutes.text = calculteAgeInMinutes(selectedDateAsString)
+    }
+
+    private fun calculteAgeInMinutes(selectedDate: String): String {
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
+
+        val selectedDate = sdf.parse(selectedDate)
+        val now = sdf.parse(sdf.format(System.currentTimeMillis()))
+        val InMinutes = (now.time / 60000) - (selectedDate.time / 60000)
+
+        return "${InMinutes}"
+
+    }
+}
